3ce6282b89afc58eed205bfe1c95d86c68ec9fe3
woodwardtw <twwoodward@me.com>
2021-12-11 07:53:29 -0500

    working on side menu

--- loop-templates/content-single-tutorial.php
+++ loop-templates/content-single-tutorial.php
@@ -28,29 +28,70 @@ defined( 'ABSPATH' ) || exit;
 	<div class="row tutorial">
 		<div class="col-md-2">
 			<div class="tutorial-nav">
-				<ul>
-					<li><a href="#intro">Intro</a></li>
-					<li><a href="#how-to">How To</a></li>
-					<li><a href="#uses">Uses</a></li>
-					<li><a href="#additional-examples">Examples</a></li>
-					<li><a href="#conclusion">Conclusion</a></li>
- 				</ul>	
-			</div>
+			<nav id="navbar-example2" class="navbar navbar-light bg-light px-3">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <a class="nav-link" href="#scrollspyHeading1">First</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#scrollspyHeading2">Second</a>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#scrollspyHeading3">Third</a></li>
+        <li><a class="dropdown-item" href="#scrollspyHeading4">Fourth</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#scrollspyHeading5">Fifth</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+				<nav id="scroll-nav" class="navbar navbar-light bg-light px-3">
+					<a class="navbar-brand" href="#">Intro</a>
+					<ul class="nav nav-pills">
+						<li class="nav-item">
+						<a class="nav-link" href="#how-to">How to</a>
+						</li>
+						<li class="nav-item">
+						<a class="nav-link" href="#scrollspyHeading2">Second</a>
+						</li>
+						<li class="nav-item">
+						<a class="nav-link" href="#apple-specific">Apple</a>      
+						</li>
+					</ul>
+				</nav>
+			</div>			
 		</div>		
-		<?php echo excel_header('introduction', 'Introduction');?>
-		<?php echo excel_example_file();?>
-		
-		<?php echo excel_header('how_to', 'How To');?>
+		<div data-bs-spy="scroll" data-bs-target="#scroll-nav">
+			<?php echo excel_header('introduction', 'Introduction');?>
+			<?php echo excel_example_file();?>
+			
+			<?php echo excel_header('how_to', 'How To');?>
 
-		<?php echo excel_header('apple_specific', 'Apple Specific');?>
+			<?php echo excel_header('apple_specific', 'Apple Specific');?>
 
-		<?php echo excel_header('uses', 'Uses');?>
+			<?php echo excel_header('uses', 'Uses');?>
 
-		<?php echo excel_syntax();?>
-		<?php echo excel_additional_examples();?>
-		<?php echo excel_header('conclusion', 'Conclusion');?>
-	
+			<?php echo excel_syntax();?>
+			<?php echo excel_additional_examples();?>
+			<?php echo excel_header('conclusion', 'Conclusion');?>
+		</div>
 		<?php //the_content(); ?>
+
+<div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+  <h4 id="scrollspyHeading1">First heading</h4>
+  <p class='big'>...</p>
+  <h4 id="scrollspyHeading2">Second heading</h4>
+  <p class='big'>...</p>
+  <h4 id="scrollspyHeading3">Third heading</h4>
+  <p>...</p>
+  <h4 id="scrollspyHeading4">Fourth heading</h4>
+  <p>...</p>
+  <h4 id="scrollspyHeading5">Fifth heading</h4>
+  <p>...</p>
+</div>
 
 		<?php
 		wp_link_pages(
