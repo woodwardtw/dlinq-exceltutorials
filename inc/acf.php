a03fc1dfadd705958094771037802da87e6050f0
woodwardtw <twwoodward@me.com>
2022-11-02 11:13:02 -0400

    typo

--- inc/acf.php
+++ inc/acf.php
@@ -16,7 +16,7 @@ function resource_image($title)
 {
     if (get_field('featured_image')) {
         $img_data = get_field('featured_image');
-        $url = $img_data['sizes']['medium-large'];
+        $url = $img_data['sizes']['medium_large'];
         if($img_data['alt']){
             $alt = $img_data['alt']; 
         } else {
