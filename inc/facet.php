f75a5c9cce82deb8e5032d865f0843839373b6b7
woodwardtw <twwoodward@me.com>
2022-11-02 11:05:33 -0400

    cleaner code for facets

--- inc/facet.php
+++ inc/facet.php
@@ -11,9 +11,7 @@ defined( 'ABSPATH' ) || exit;
 while ( have_posts() ): the_post(); ?>
     <div class="col-md-3">
         <div class="card position-relative">
-            <a href="<?php the_permalink(); ?>">
                 <?php echo resource_image();?>
-            </a>
           <div class="card-body">
             <a href="<?php the_permalink(); ?>" class="stretched-link"><?php the_title(); ?></a>        
           </div>
